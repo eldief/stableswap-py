3e2ca1f9958374e47d69eace33675943e9355443
LDF <l.defilippi@protonmail.com>
2023-07-07 15:06:09 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stableswap-py",
+    version="0.1",
+    packages=find_packages(),
+    author="eldief",
+    author_email="",
+    description="A short description of your package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/eldief/stableswap-py",
+)
